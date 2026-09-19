0c80a2dd884892dbcf3372cf492292f057d32bdd
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 07:00:44 +0300

    chore: migrate ESLint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "src/generated/prisma/**",
+  ]),
+]);
