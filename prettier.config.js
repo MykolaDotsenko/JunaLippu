229b56eae739cc82a478f152fc888838ff09e2c9
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 07:02:28 +0300

    chore: remove legacy Tailwind Prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @type {import("prettier").Config} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 100,
 };
 
 export default config;
